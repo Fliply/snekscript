e9d10cd0664473acea8b78fa929a2625ff6fdba1
Fliply <62617504+Fliply@users.noreply.github.com>
2022-05-24 19:23:00 +0000

    Delete shell.py

--- shell.py
+++
@@ -1,8 +0,0 @@
-import snekscript
-
-while True:
-    text = input('snekscript > ')
-    result, error = snekscript.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
